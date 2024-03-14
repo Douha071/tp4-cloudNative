35caa1f8c2b84e697bdc0c84af6259379c139904
Douha071 <162474270+Douha071@users.noreply.github.com>
2024-03-14 18:06:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const cors = require('cors');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
 mongoose.connect(process.env.URL_MONGOOSE, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
-// Routes
+
 app.use('/users', require('./Routes/user'));
 app.use('/auteurs', require('./Routes/auteur'));
 app.use('/editeurs', require('./Routes/editeur'));
